389507f20614a89e0bb7d306e6234578575de59f
divyanjalimadhupada <155824889+divyanjaliMadhupada08@users.noreply.github.com>
2024-07-23 15:46:51 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import LoginObjectState from './LoginObjectState';
+import ListGroup from "./ListGroup";
+import ListGroupProps from "./ListGroupProps";
+import Calculator from './Calculator';
 
 function App() {
- 
+  let teachers=['siva','vamsi','navya','divya','shan']
   return (
-      <LoginObjectState/>
+    <ListGroupProps items={teachers} heading="LIst of Teachers"/>
+    
   )
 }
 
