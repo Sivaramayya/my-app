330b9a7e63e3a1297a829eac8076753d2e430941
divyanjalimadhupada <155824889+divyanjaliMadhupada08@users.noreply.github.com>
2024-08-03 17:32:31 +0530

    checking app,js with other files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+<<<<<<< Updated upstream
 import 'bootstrap/dist/css/bootstrap.css';
 import ListGroup from "./ListGroup";
 import ListGroupProps from "./ListGroupProps";
 import Calculator from './Calculator';
+=======
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes';
+import { UserProvider } from './context/UserContext';
+import  Calculator  from './Calculator';
+>>>>>>> Stashed changes
 
 function App() {
   let teachers=['siva','vamsi','navya','divya','shan']
   return (
+<<<<<<< Updated upstream
     <ListGroupProps items={teachers} heading="LIst of Teachers"/>
     
   )
 }
+=======
+   // <UserProvider>
+     // <Router>
+       // <AppRoutes />
+      //</Router>
+    //</UserProvider>
+    <Calculator/>
+  );
+};
+>>>>>>> Stashed changes
 
 export default App;
